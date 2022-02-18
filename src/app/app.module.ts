bbe4adebf0db8d68dea3a992184eded5be6cabec
Mohith <mohith4krish@gmail.com>
2022-02-18 21:01:48 +0530

    feature two added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturetwoComponent } from './featuretwo/featuretwo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturetwoComponent
   ],
   imports: [
     BrowserModule,
